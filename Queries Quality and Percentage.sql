fdcb667a2f03a5a62f2b9279fe67d382fb3ee5d5
Scott <scott19971003@gmail.com>
2025-03-18 20:30:48 +1000

    Update Queries Quality and Percentage.sql

--- Queries Quality and Percentage.sql
+++ Queries Quality and Percentage.sql
@@ -26,3 +26,11 @@ round(count(case when rating < 3 then 1 else null end)*100/count(qery_name), 2) 
 from leetcode.queries
 where qery_name is not null
 group by qery_name;
+
+# Better solution
+select 
+    query_name, 
+    round(AVG(rating/position), 2) as quality,
+    round(AVG(rating<3)*100, 2) as poor_query_percentage
+from Queries
+group by query_name;
